ffd2af868e4f3c00a682f7c23a6980fb2071c161
Bekarys <magauiyanov.b@gmail.com>
2023-10-21 18:07:47 +0600

    genre filled and changed permissions for list and retrieve and i need rest for 20 mins

--- apps/author/views.py
+++ apps/author/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 from .models import Author
 from .serializers import AuthorSerializer
@@ -8,5 +8,11 @@ from .serializers import AuthorSerializer
 class AuthorViewSet(viewsets.ModelViewSet):  # JUST FOR ADMIN USERS
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-    permission_classes = [IsAdminUser]
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
 
--- apps/genre/views.py
+++ apps/genre/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
-# Create your views here.
+from .models import Genre
+from .serializers import GenreSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):  # JUST FOR ADMIN USERS
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+
